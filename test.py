b886019171a343fbef8f0d8330bfed774617f822
DaraiB <113297682+DaraiB@users.noreply.github.com>
2023-10-04 11:29:08 +0500

    Update test.py

--- test.py
+++ test.py
@@ -17,6 +17,7 @@ model.add(Conv2D(32, (3, 3), input_shape=input_shape))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
+
 model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
